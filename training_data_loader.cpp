154d8f52d94876992f8d9a063f80ed6400727b8b
Linmiao Xu <linmiao.xu@gmail.com>
2024-01-13 14:42:48 -0500

    Fix logic for skipping for minimized binpacks

--- training_data_loader.cpp
+++ training_data_loader.cpp
@@ -868,7 +868,7 @@ std::function<bool(const TrainingDataEntry&)> make_skip_predicate(bool filtered,
 
             auto do_filter = [&]() {
                 // need to skip BM captures with score == 0 for compatibility with min-v1 binpacks
-                return e.isInCheck() || (e.score != 0 && e.isCapturingMove() && e.seeGE(0));
+                return e.isInCheck() || (e.isCapturingMove() && (e.score == 0 || e.seeGE(0)));
             };
 
             // Allow for predermined filtering without the need to remove positions from the dataset.
