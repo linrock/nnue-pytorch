a3391b590013f54f995f301ee8215dbf26582a03
Linmiao Xu <linmiao.xu@gmail.com>
2024-05-20 03:19:30 -0400

    pawnBuckets-torch-compile

--- training_data_loader.cpp
+++ training_data_loader.cpp
@@ -435,7 +435,9 @@ private:
         is_white[i] = static_cast<float>(e.pos.sideToMove() == Color::White);
         outcome[i] = (e.result + 1.0f) / 2.0f;
         score[i] = e.score;
-        psqt_indices[i] = (e.pos.piecesBB().count() - 1) / 4;
+        // psqt_indices[i] = (e.pos.piecesBB().count() - 1) / 4;
+        psqt_indices[i] = ((pos.piecesBB() & (pos.piecesBB(Piece(PieceType::Pawn, Color::White)) | pos.piecesBB(Piece(PieceType::Pawn, Color::Black)))) - 1) / 2;
+
         layer_stack_indices[i] = psqt_indices[i];
         fill_features(FeatureSet<Ts...>{}, i, e);
     }
