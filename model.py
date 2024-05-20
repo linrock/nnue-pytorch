6de914609535f1a133245985b27320f6b13b29d6
Linmiao Xu <linmiao.xu@gmail.com>
2024-05-20 12:00:45 -0400

    L1-3072

--- model.py
+++ model.py
@@ -10,7 +10,7 @@ from feature_transformer import DoubleFeatureTransformerSlice
 torch.set_float32_matmul_precision("high")
 
 # 3 layer fully connected network
-L1 = 4096
+L1 = 3072
 L2 = 15
 L3 = 32
 
