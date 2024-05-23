356c62041a845ab374fba054ecb2ebcca4a8129d
Linmiao Xu <linmiao.xu@gmail.com>
2024-05-22 23:25:10 -0400

    15% more loss when Q too high

--- model.py
+++ model.py
@@ -328,7 +328,7 @@ class NNUE(pl.LightningModule):
     pt = pf * actual_lambda + t * (1.0 - actual_lambda)
 
     loss = torch.pow(torch.abs(pt - qf), 2.5)
-    loss = loss * ((qf > pt) * 0.1 + 1)
+    loss = loss * ((qf > pt) * 0.15 + 1)
     loss = loss.mean()
 
     return loss
