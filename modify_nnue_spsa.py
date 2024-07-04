9ed57370cb50c1eec708d3108c6dc8c402fdf307
Linmiao Xu <linmiao.xu@gmail.com>
2024-07-04 14:46:25 -0400

    Print spsa status from page

--- modify_nnue_spsa.py
+++ modify_nnue_spsa.py
@@ -20,15 +20,20 @@ def modify_nnue(nnue_filename, spsa_page_url):
     print(spsa_page_url)
     response = requests.get(spsa_page_url)
     soup = BeautifulSoup(response.text, "html.parser")
+
+    spsa_status_el = soup.find("div", {"class": "elo-results-top"})
+    for row in spsa_status_el.text.strip().split("\n"):
+        if row.strip():
+            print(" ", row.strip())
+
     spsa_params_table = soup.find_all("table")[1]
+    params_rows = spsa_params_table.find_all("tr", class_="spsa-param-row")
+    print(f"Found {len(params_rows)} spsa params")
 
     feature_set = features.get_feature_set_from_name("HalfKAv2_hm")
     with open(nnue_filename, "rb") as f:
         reader = NNUEReader(f, feature_set)
         model = reader.model
-
-    params_rows = spsa_params_table.find_all("tr", class_="spsa-param-row")
-    print(f"Found {len(params_rows)} spsa params")
 
     for row in params_rows:
         td = row.find_all("td")
@@ -37,11 +42,11 @@ def modify_nnue(nnue_filename, spsa_page_url):
 
         if param_name.startswith("twoW"):
             param_type, bucket, idx1, idx2 = param_name.replace("[", " ").replace("]", " ").split()
-            model.layer_stacks.l2.weight.data[32*int(bucket) + int(idx1), int(idx2)] = float(value) / 64
+            model.layer_stacks.l2.weight.data[32*int(bucket) + int(idx1), int(idx2)] = value / 64
 
         elif param_name.startswith("ftB"):
             param_type, idx = param_name.replace("[", " ").replace("]", " ").split()
-            model.input.bias.data[int(idx)] = float(value) / 254
+            model.input.bias.data[int(idx)] = value / 254
 
     description = "Network trained with the https://github.com/official-stockfish/nnue-pytorch trainer."
     writer = NNUEWriter(model, description, ft_compression="leb128")
