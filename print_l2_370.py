250efb70b2922b95e7083599ad4517c622781c33
Linmiao Xu <linmiao.xu@gmail.com>
2024-07-08 22:39:07 -0400

    Print twoW 370 params

---
+++ print_l2_370.py
@@ -0,0 +1,31 @@
+import hashlib
+import os
+from pathlib import Path
+import sys
+
+from bs4 import BeautifulSoup
+import requests
+
+import features
+from serialize import NNUEReader, NNUEWriter
+
+
+def print_spsa_params(nnue_filename):
+    feature_set = features.get_feature_set_from_name("HalfKAv2_hm")
+    with open(nnue_filename, "rb") as f:
+        model = NNUEReader(f, feature_set).model
+
+    c_end = 6
+    for i in range(8):
+        for j in range(32):
+            for k in range(30):
+                value = int(model.layer_stacks.l2.weight[32 * i + j, k] * 64)
+                if abs(value) >= 50:
+                    print(f"twoW[{i}][{j}][{k}],{value},-127,127,{c_end},0.0020")
+
+
+if __name__ == "__main__":
+    filename = "nnue/nn-ddcfb9224cdb.nnue"
+    filename = "nnue/nn-74f1d263ae9a.nnue"
+    # filename = "nnue/nn-e8bac1c07a5a.nnue"
+    print_spsa_params(filename)
