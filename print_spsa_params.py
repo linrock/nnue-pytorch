a8e8140f2a4a4e97425683d16db06e76fd52c04a
Linmiao Xu <linmiao.xu@gmail.com>
2024-07-29 13:42:35 -0400

    Rename script for clarity

--- print_spsa_params.py
+++ print_spsa_params.py
@@ -15,7 +15,7 @@ def prep_ft_biases(model):
     num_biases = 0
     for i,value in enumerate(model.input.bias.data[:3072]):
         value_int = int(value * 254)
-        if abs(value_int) > 250:
+        if abs(value_int) > 175:
             print(f"ftB[{i}],{value_int},-1024,1024,{c_end},0.0020")
             num_biases += 1
     return num_biases
@@ -28,7 +28,7 @@ def prep_l2_weights(model):
         for j in range(32):
             for k in range(30):
                 value = int(model.layer_stacks.l2.weight[32 * i + j, k] * 64)
-                if abs(value) >= 10:
+                if abs(value) <= 1:
                     print(f"twoW[{i}][{j}][{k}],{value},-127,127,{c_end},0.0020")
                     num_weights += 1
     # print(f"# weights to tune: {num_weights}")
@@ -50,6 +50,19 @@ def prep_l2_weights_stack0(model):
 
     #     print()
     # print(f"# weights to tune: {num_weights}")
+
+
+def prep_l1_weights(model):
+    c_end = 6
+    num_weights = 0
+    for i in range(128):
+        for j in range(3072):
+            value = int(model.layer_stacks.l2.weight[32 * i + j, k] * 64)
+            if abs(value) == 0:
+                print(f"twoW[{i}][{j}][{k}],{value},-127,127,{c_end},0.0020")
+                num_weights += 1
+    # print(f"# weights to tune: {num_weights}")
+    return num_weights
 
 
 def print_spsa_owb(model):
@@ -248,7 +261,7 @@ def print_spsa_params_all(model):
     print(f"# biases to tune:  {num_biases}")
 
 
-def prep_spsa_params(nnue_filename):
+def print_spsa_params(nnue_filename):
     feature_set = features.get_feature_set_from_name("HalfKAv2_hm")
     with open(nnue_filename, "rb") as f:
         reader = NNUEReader(f, feature_set)
@@ -265,4 +278,5 @@ def prep_spsa_params(nnue_filename):
 
 if __name__ == "__main__":
     # prep_spsa_params("nnue/nn-ddcfb9224cdb.nnue")
-    prep_spsa_params("nnue/nn-e8bac1c07a5a.nnue")
+    # prep_spsa_params("nnue/nn-e8bac1c07a5a.nnue")
+    print_spsa_params("nnue/nn-31337bea577c.nnue")
