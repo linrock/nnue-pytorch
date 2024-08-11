efc407b9da147e0bf8f1b6498b581864412b97f2
Linmiao Xu <linmiao.xu@gmail.com>
2024-08-11 15:32:18 -0400

    Fix base net when apply spsa params

--- apply_spsa_to_nnue.py
+++ apply_spsa_to_nnue.py
@@ -160,7 +160,8 @@ if __name__ == "__main__":
 
     # nnue_filename = "nnue/nn-ddcfb9224cdb.nnue"
     # nnue_filename = "nnue/nn-74f1d263ae9a.nnue"
-    nnue_filename = "nnue/nn-e8bac1c07a5a.nnue"
+    # nnue_filename = "nnue/nn-e8bac1c07a5a.nnue"
+    nnue_filename = "nnue/nn-31337bea577c.nnue"
 
     spsa_page_url = sys.argv[1]
     print(f"Modifying {nnue_filename.split('/')[-1]} ...")
