71de2861e2afe9b1c32bf4f1a13586966882cf91
Linmiao Xu <linmiao.xu@gmail.com>
2024-10-24 18:56:36 -0400

    Try clamping range

--- training_data_loader.cpp
+++ training_data_loader.cpp
@@ -436,7 +436,7 @@ private:
         is_white[i] = static_cast<float>(e.pos.sideToMove() == Color::White);
         outcome[i] = (e.result + 1.0f) / 2.0f;
         score[i] = e.score;
-        psqt_indices[i] = e.pos.totalMaterial() / 2495;
+        psqt_indices[i] = std::clamp(e.pos.totalMaterial() / 2500, 0, 7);
         layer_stack_indices[i] = psqt_indices[i];
         fill_features(FeatureSet<Ts...>{}, i, e);
     }
