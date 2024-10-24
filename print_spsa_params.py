af4a55711b1509b6ea7beb8541807c6fd7e860ae
Linmiao Xu <linmiao.xu@gmail.com>
2024-10-24 18:33:19 -0400

    Enable random subset of L2

--- print_spsa_params.py
+++ print_spsa_params.py
@@ -1,3 +1,4 @@
+import random
 import sys
 
 import features
@@ -73,13 +74,14 @@ if __name__ == "__main__":
         sys.exit(0)
 
     n = NnueSpsaParamPrinter(sys.argv[1])
-    # n.print_ft_biases(lambda value: abs(value) < 50)
+    n.print_ft_biases(lambda value: abs(value) < 30)
 
     # n.print_l1_weights(lambda value: abs(value) == 0)
     # n.print_l1_biases()
 
-    # n.print_l2_weights(lambda value: abs(value) > 50)
+    # random.seed(0)
+    # n.print_l2_weights(lambda value: random.randint(0, 12) == 0)
     # n.print_l2_biases()
 
-    n.print_output_weights()
-    n.print_output_biases()
+    # n.print_output_weights()
+    # n.print_output_biases()
