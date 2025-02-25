54fb9b894ae3f44e691b0830b3c044f6c0590fe2
Linmiao Xu <linmiao.xu@gmail.com>
2025-02-25 00:53:05 -0500

    Power loss 2.2

--- model.py
+++ model.py
@@ -326,7 +326,7 @@ class NNUE(pl.LightningModule):
     actual_lambda = self.start_lambda + (self.end_lambda - self.start_lambda) * (self.current_epoch / self.max_epoch)
     pt = pf * actual_lambda + t * (1.0 - actual_lambda)
 
-    loss = torch.pow(torch.abs(pt - qf), 2.5).mean()
+    loss = torch.pow(torch.abs(pt - qf), 2.2).mean()
 
     return loss
 
